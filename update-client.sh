df48c4f8d6c89b742a4a83c5b6d43e847fa95988
Bobby Walker <gwaland@gmail.com>
2014-08-16 14:11:45 -0500

    more updates

--- update-client.sh
+++ update-client.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+#rsync -avrc -e ssh piu-server:~/Songs/* /home/piu/Songs/
+#cp -rsf /home/piu/Songs/ /home/piu/sm5/
 
+progress()
+{
+        local pid=$1
+        local delay=0.75
+        local spinstr='|/-\'
+        while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+		local out=$(tail -1 /tmp/prog | strings | awk '{print $2}' | tr -d '%')
+		echo $out
+		sleep 5
+        done
+}
+rsync -azr --info=progress2 -e ssh piu-server:~/Songs/* /home/piu/Songs/ > /tmp/prog  &
+progress $! #| uniq #| whiptail --title "Son Updates" --gauge "Downloading new songs" 20 70 0
