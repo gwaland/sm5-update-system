518b63d323b48dc299f87861690ef336c407919d
piu <piu@piu-beta.home.priv>
2014-08-16 18:26:18 -0500

    fixed whiptail issue for rsync. need to implement check for versions.

--- update-client.sh
+++ update-client.sh
@@ -8,10 +8,13 @@ progress()
         local delay=0.75
         local spinstr='|/-\'
         while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-		local out=$(tail -1 /tmp/prog | strings | awk '{print $2}' | tr -d '%')
+		local out=$(tail -1 /tmp/prog | strings|tail -1 | awk '{print $2}' | tr -d '%')
 		echo $out
-		sleep 5
+		sleep 3
         done
 }
 rsync -azr --info=progress2 -e ssh piu-server:~/Songs/* /home/piu/Songs/ > /tmp/prog  &
-progress $! #| uniq #| whiptail --title "Son Updates" --gauge "Downloading new songs" 20 70 0
+progress $! |  whiptail --title "Song Updates" --gauge "Downloading new songs" 20 70 0
+cp -rsf /home/piu/Songs /home/piu/sm5/
+
+
