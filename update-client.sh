138fcaf1b9ee54dbdbf0aa1bf32770073f053ce1
Bobby Walker <gwaland@gmail.com>
2014-08-16 21:48:23 -0500

    initial stepmania update code added.

--- update-client.sh
+++ update-client.sh
@@ -13,8 +13,33 @@ progress()
 		sleep 3
         done
 }
-rsync -azr --info=progress2 -e ssh piu-server:~/Songs/* /home/piu/Songs/ > /tmp/prog  &
-progress $! |  whiptail --title "Song Updates" --gauge "Downloading new songs" 20 70 0
+#update songs from server. 
+rsync -Lazr --info=progress2 -e ssh piu-server:~/Songs/* /home/piu/Songs/ > /tmp/prog  &
+progress $! |  whiptail --title "Song Updates" --gauge "Downloading new songs" 7 70 0
 cp -rsf /home/piu/Songs /home/piu/sm5/
 
+#update stepmania from server.
+rsync -Lazr --info=progress2 -e ssh piu-server:/var/www/html/*-current* /home/piu/new > /tmp/prog &
+progress $! | whiptail --title "Stepmania update" --gauge "Downloading Stepmania Updates" 7 70 0
+
+
+cmp -s /home/piu/new/stepmania-build-current.md5sum /home/piu/installed/stepmania-build-current.md5sum 
+if [ $? -eq 0 ]; then
+	echo "SM is up to date!"
+else
+	echo "installing updated Stepmania."
+	mv /home/piu/sm5 /home/piu/sm5.backup
+	mkdir /home/piu/sm5
+	cd /home/piu/sm5
+	tar xf /home/piu/new/stepmania-build-current.tar.gz
+	rm -rf /home/piu/sm5/Save
+	touch portable.ini
+	mkdir Save
+	cp -rf /home/piu/sm5.backup/Save/* /home/piu/sm5/Save/
+	cp -rsf /home/piu/Songs /home/piu/sm5
+	cp /home/piu/new/stepmania-build-current.md5sum /home/piu/installed/stepmania-build-current.md5sum
+        ls -1 /home/piu/sm5/Themes > /home/piu/sm5/theme.exclude
+	rsync -avr --exclude-from /home/piu/sm5/theme.exclude /home/piu/sm5.backup/Themes/ /home/piu/sm5/Themes/
+
+fi
 
