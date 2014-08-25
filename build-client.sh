22528ec17e183bc1c73f18ae0e12c3b441dc2780
Bobby Walker <gwaland@gmail.com>
2014-08-24 20:20:20 -0500

    corrected packages and an issue with ssh keys being pushed to the server.

--- build-client.sh
+++ build-client.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-_PACKAGES='binutils git libmad0 libvorbisfile3 nvidia-173 libglu1-mesa libglew1.10 libjack0 xserver-xorg xinit'
-_SERVER="piu-server"
-_SERVER_USER="piu"
-
+_PACKAGES='binutils git libmad0 libvorbisfile3 nvidia-173 libglu1-mesa libglew1.10 libjack0 xserver-xorg xinit alsa'
+_SERVER=piu-server
+_SERVER_USER=piu
+_USER=$(whoami)
 ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub | ssh $SERVER_USER@$_SERVER "cat >> ~/.ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | ssh $_SERVER_USER@$_SERVER "cat >> ~/.ssh/authorized_keys"
 
 ln -s ~/sm5-update-system/update-client.sh ~/update.sh
 mkdir ~/Songs
@@ -20,7 +20,7 @@ sudo sed -i s/exit 0//g /etc/rc.local
 sudo  "echo 'chmod a+wrx /dev/piuio0' >> /etc/rc.local"
 sudo "echo 'exit 0' >> /etc/rc.local"
 sudo apt-get update
-sudo apt-get install $_PACKAGES
+sudo apt-get -y install $_PACKAGES
 sudo sed -i s/exec/#exec/g /etc/init/tty1.conf
 sudo "echo 'exec /bin/login -f piu tty1 </dev/tty1 >/dev/tty1 2>&1' >> /etc/init/tty1.conf"
 echo 'exec /home/piu/stepmania.sh' > ~/.xinitrc; chmod +x ~/.xinitrc
@@ -33,4 +33,5 @@ echo '    startx' >> ~/.profile
 echo 'fi' >> ~/.profile
 
 ~/update.sh
-
+sudo usermod -G audio $_USER
+sudo alsa force-reload
