36f62632bd915e3c916c191b175cd65af67c95ee
Bobby Walker <gwaland@gmail.com>
2014-09-03 13:48:27 -0500

    cleaning up update-client.sh for automated deployment.

--- update-client.sh
+++ update-client.sh
@@ -33,25 +33,26 @@ updateclient $_REMOTE_SONG_PATH $_LOCAL_SONG_PATH 'Songs'
 cp -rsf /home/piu/Songs /home/piu/sm5/
 
 #update stepmania from server.
-updateclient $_REMOTE_SM_PATH $_LOCAL_UPDATE_PATH 'Stepmania Software'
+updateclient $_REMOTE_SM_PATH $_LOCAL_UPDATE_PATH/sm5 'Stepmania Software'
 
-cmp -s /home/piu/new/stepmania-build-current.md5sum /home/piu/installed/stepmania-build-current.md5sum 
+cmp -s $_LOCAL_UPDATE_PATH/sm5/stepmania-build-current.md5sum $_LOCAL_UPDATE_PATH/installed/stepmania-build-current.md5sum 
 if [ $? -eq 0 ]; then
 	echo "SM is up to date!"
 else
+	_NOW=$(date +%Y%m%d%H%M)
 	echo "installing updated Stepmania."
-	mv /home/piu/sm5 /home/piu/sm5.backup
-	mkdir /home/piu/sm5
-	cd /home/piu/sm5
-	tar xf /home/piu/new/stepmania-build-current.tar.gz
-	rm -rf /home/piu/sm5/Save
+	mv $_LOCAL_SM_PATH $_LOCAL_BACKUP_PATH/sm5.$_NOW
+	mkdir $_LOCAL_SM_PATH
+	cd $_LOCAL_SM_PATH
+	tar xf $_LOCAL_UPDATE_PATH/sm5/stepmania-build-current.tar.gz
+	rm -rf $_LOCAL_SM_PATH/Save
 	touch portable.ini
 	mkdir Save
-	cp -rf /home/piu/sm5.backup/Save/* /home/piu/sm5/Save/
-	cp -rsf /home/piu/Songs /home/piu/sm5
-	cp /home/piu/new/stepmania-build-current.md5sum /home/piu/installed/stepmania-build-current.md5sum
-        ls -1 /home/piu/sm5/Themes > /home/piu/sm5/theme.exclude
-	rsync -avr --exclude-from /home/piu/sm5/theme.exclude /home/piu/sm5.backup/Themes/ /home/piu/sm5/Themes/
+	cp -rf $_LOCAL_BACKUP_PATH/sm5.$_NOW/Save/* $_LOCAL_SM_PATH/Save/
+	cp -rsf $_LOCAL_SONG_PATH/* $_LOCAL_SM_PATH/Songs/
+	cp $_LOCAL_UPDATE_PATH/sm5/stepmania-build-current.md5sum $_LOCAL_UPDATE_PATH/installed/stepmania-build-current.md5sum
+        ls -1 $_LOCAL_SM_PATH/Themes > $_LOCAL_SM_PATH/theme.exclude
+	rsync -avr --exclude-from $_LOCAL_SM_PATH/theme.exclude $_LOCAL_BACKUP_PATH/sm5.$_NOW/Themes/ $_LOCAL_SM_PATH/Themes/
 
 fi
 
