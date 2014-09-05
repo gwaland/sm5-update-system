4d6dbe62dad7070ce8d2632372ae050a26fccd91
Bobby Walker <gwaland@gmail.com>
2014-09-04 22:01:48 -0500

    configure to not use piu as default user and minor bugfixes in configuration building.

--- build-client.sh
+++ build-client.sh
@@ -37,8 +37,8 @@ spinner $!
 
 #get server settings.
 _SERVER=$(whiptail --inputbox "What is the IP or hostname of the Server?" 8 78 sm5-server --title "Server Name" 3>&1 1>&2 2>&3)
-ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
-ssh-keyscan -H $_SERVER >> ~/.ssh/known_hosts
+ssh-keygen -q -t rsa -N "" -f $HOME/.ssh/id_rsa
+ssh-keyscan -H $_SERVER >> $HOME/.ssh/known_hosts
 
 _EXIT_STATUS=1
 while [ $_EXIT_STATUS -ne 0 ]; do
@@ -48,7 +48,7 @@ while [ $_EXIT_STATUS -ne 0 ]; do
         sshpass -e ssh $_SERVER_USER@$_SERVER echo
         _EXIT_STATUS=$?
 done
-cat ~/.ssh/id_rsa.pub | sshpass -e ssh $_SERVER_USER@$_SERVER "cat >> ~/.ssh/authorized_keys"
+cat $HOME/.ssh/id_rsa.pub | sshpass -e ssh $_SERVER_USER@$_SERVER "cat >> ~/.ssh/authorized_keys"
 
 #get server settings. 
 scp $_SERVER_USER@$_SERVER:~/.sm5-server.rc /tmp/serversettings
@@ -65,10 +65,10 @@ fi
 
 unset SSHPASS
 
-_LOCAL_UPDATE_PATH=$(whiptail --inputbox "Path to store update files?" 8 78 /home/$_USER/new --title "Update Path" 3>&1 1>&2 2>&3)
-_LOCAL_SONG_PATH=$(whiptail --inputbox "Path to store song files?" 8 78 /home/$_USER/Songs --title "Song Path" 3>&1 1>&2 2>&3)
-_LOCAL_BACKUP_PATH=$(whiptail --inputbox "Path to store Backup files?" 8 78 /home/$_USER/backup --title "Backup Path" 3>&1 1>&2 2>&3)
-_LOCAL_SM_PATH=$(whiptail --inputbox "Path to store stepmania files?" 8 78 /home/$_USER/sm5 --title "Stepmania Path" 3>&1 1>&2 2>&3)
+_LOCAL_UPDATE_PATH=$(whiptail --inputbox "Path to store update files?" 8 78 $HOME/new --title "Update Path" 3>&1 1>&2 2>&3)
+_LOCAL_SONG_PATH=$(whiptail --inputbox "Path to store song files?" 8 78 $HOME/Songs --title "Song Path" 3>&1 1>&2 2>&3)
+_LOCAL_BACKUP_PATH=$(whiptail --inputbox "Path to store Backup files?" 8 78 $HOME/backup --title "Backup Path" 3>&1 1>&2 2>&3)
+_LOCAL_SM_PATH=$(whiptail --inputbox "Path to store stepmania files?" 8 78 $HOME/sm5 --title "Stepmania Path" 3>&1 1>&2 2>&3)
 
 
 
@@ -78,55 +78,70 @@ _LOCAL_SM_PATH=$(whiptail --inputbox "Path to store stepmania files?" 8 78 /home
 #echo "We're going to install the ssh key on the server.  You'll need the password for your server's $_SERVER_USER now."
 #cat ~/.ssh/id_rsa.pub | ssh $_SERVER_USER@$_SERVER "cat >> ~/.ssh/authorized_keys"
 
-ln -s ~/sm5-update-system/update-client.sh ~/update.sh
+ln -s ~/sm5-update-system/update-client.sh $HOME/update.sh
 mkdir -p $_LOCAL_SONG_PATH
 mkdir -p $_LOCAL_SM_PATH/Themes
 mkdir -p $_LOCAL_SM_PATH/Save
 mkdir -p $_LOCAL_UPDATE_PATH
 mkdir -p $_LOCAL_BACKUP_PATH
 
-#echo "Doing all the sudo stuff needed in one chunk.  Put in your $(whoami) password here."
+# take care of persistant rules issues. 
 echo -e "$PASSWORD\n" | sudo -S ln -sf /dev/null /etc/udev/rules.d/70-persistent-net.rules
+
+#setup grub to work with 640x480 screen
 echo -e "$PASSWORD\n" | sudo -S sed -i s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"nomodeset\"/g /etc/default/grub
 echo -e "$PASSWORD\n" | sudo -S sed -i s/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/g /etc/default/grub
 echo -e "$PASSWORD\n" | sudo -S sed -i s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=640x480/g /etc/default/grub
 echo -e "$PASSWORD\n" | sudo -S update-grub
+
+#This is for legacy PIUIO drivers.
 echo -e "$PASSWORD\n" | sudo -S sed -i s/exi/#exi/g /etc/rc.local
 echo -e "$PASSWORD\n" | sudo -S sh -c 'echo chmod a+wrx /dev/piuio0 >> /etc/rc.local'
 echo -e "$PASSWORD\n" | sudo -S sh -c 'echo exit 0 >> /etc/rc.local'
+
+#this will force tty1 to login as the primary user.
 echo -e "$PASSWORD\n" | sudo -S sed -i s/exec/#exec/g /etc/init/tty1.conf
-echo -e "$PASSWORD\n" | sudo -S sh -c 'echo "exec /bin/login -f piu tty1 </dev/tty1 >/dev/tty1 2>&1" >> /etc/init/tty1.conf'
+echo -e "$PASSWORD\n" | sudo -S sh -c "echo "exec /bin/login -f $_USER tty1 </dev/tty1 >/dev/tty1 2>&1" >> /etc/init/tty1.conf"
+
+#make sure our user has access to the audio controls
 echo -e "$PASSWORD\n" | sudo -S usermod -a -G audio $_USER
 
-#echo updating apt.
-#sudo apt-get -qq update > /dev/null 2>&1 &
-#spinner $!
+#install required packages for this to run. 
 echo installing packages.
 echo -e "$PASSWORD\n" | sudo -S apt-get -qq -y install $_PACKAGES > /dev/null 2>&1 &
 spinner $!
 
+#force audio drivers to load
 echo -e "$PASSWORD\n" | sudo -S alsa force-reload
 
-echo 'exec /home/piu/stepmania.sh' > ~/.xinitrc; chmod +x ~/.xinitrc
-echo '#!/bin/bash' > ~/stepmania.sh; echo 'cd ~/sm5' >> ~/stepmania.sh; echo './stepmania' >> ~/stepmania.sh; chmod +x ~/stepmania.sh
-echo 'if (  last | grep -e $(whoami) -e reboot | head -2 | grep -q reboot ); then' >> ~/.profile
-echo '     ~/update.sh' >> ~/.profile
-echo 'fi' >> ~/.profile
-echo 'if [ -z "$DISPLAY" ] && [ $(tty) == /dev/tty1 ]; then' >> ~/.profile
-echo '    startx' >> ~/.profile
-echo 'fi' >> ~/.profile
+#create the xinitrc that will start stepmania
+echo "exec $HOME/stepmania.sh" > $HOME/.xinitrc; chmod +x $HOME/.xinitrc
+
+#create the stepmania configuration. 
+echo '#!/bin/bash' > $HOME/stepmania.sh; echo 'cd ~/sm5' >> $HOME/stepmania.sh; echo './stepmania' >> $HOME/stepmania.sh; chmod +x $HOME/stepmania.sh
+
+#check to see if this is the first login.  If so, then run the updater
+echo 'if (  last | grep -e $(whoami) -e reboot | head -2 | grep -q reboot ); then' >> $HOME/.profile
+echo "     $HOME/update.sh" >> $HOME/.profile
+echo 'fi' >> $HOME/.profile
+
+#if on tty1 startx. 
+echo 'if [ -z "$DISPLAY" ] && [ $(tty) == /dev/tty1 ]; then' >> $HOME/.profile
+echo '    startx' >> $HOME/.profile
+echo '    exit' >> $HOME/.profile
+echo 'fi' >> $HOME/.profile
 
 
 #build ~/.sm5-client.rc
-echo "_SERVER=$_SERVER" > ~/.sm5-client.rc
-echo "_REMOTE_SONG_PATH=$_REMOTE_SONG_PATH'" >> ~/.sm5-client.rc
-echo "_REMOTE_THEME_PATH=$_REMOTE_THEME_PATH" >> ~/.sm5-client.rc
-echo "_REMOTE_SM_PATH=$_REMOTE_SM_PATH*" >> ~/.sm5-client.rc
-echo "_REMOTE_PIUIO_PATH=$_REMOTE_PIUIO_PATH" >> ~/.sm5-client.rc
-echo "_LOCAL_UPDATE_PATH=$_LOCAL_UPDATE_PATH" >> ~/.sm5-client.rc
-echo "_LOCAL_SONG_PATH=$_LOCAL_SONG_PATH" >> ~/.sm5-client.rc
-echo "_LOCAL_SM_PATH=$_LOCAL_SM_PATH" >> ~/.sm5-client.rc
+echo "_SERVER=$_SERVER" > $HOME/.sm5-client.rc
+echo "_REMOTE_SONG_PATH=$_REMOTE_SONG_PATH" >> $HOME/.sm5-client.rc
+echo "_REMOTE_THEME_PATH=$_REMOTE_THEME_PATH" >> $HOME/.sm5-client.rc
+echo "_REMOTE_SM_PATH=$_REMOTE_SM_PATH" >> $HOME/.sm5-client.rc
+echo "_REMOTE_PIUIO_PATH=$_REMOTE_PIUIO_PATH" >> $HOME/.sm5-client.rc
+echo "_LOCAL_UPDATE_PATH=$_LOCAL_UPDATE_PATH" >> $HOME/.sm5-client.rc
+echo "_LOCAL_SONG_PATH=$_LOCAL_SONG_PATH" >> $HOME/.sm5-client.rc
+echo "_LOCAL_SM_PATH=$_LOCAL_SM_PATH" >> $HOME/.sm5-client.rc
 
 
 
-~/update.sh
+$HOME/update.sh
