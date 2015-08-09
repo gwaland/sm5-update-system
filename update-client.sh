28c6d9c3b171d60d83130debcedf7ad51f9b164f
Bobby Walker <gwaland@gmail.com>
2015-08-09 17:00:06 -0500

    started making use of the ~/.stepmania-5.0/ to simplify updates.

--- update-client.sh
+++ update-client.sh
@@ -30,8 +30,8 @@ progress $! |  whiptail --title "$3 Updates" --gauge "Downloading new $3" 7 70 0
 
 
 #update Songs and create the symlinks. 
-updateclient $_REMOTE_SONG_PATH/ $_LOCAL_SONG_PATH 'Songs'
-cp -rsf /home/piu/Songs /home/piu/sm5/
+updateclient $_REMOTE_SONG_PATH/ $_LOCAL_SM_SHARE_PATH/Songs 'Songs'
+#cp -rsf /home/piu/Songs /home/piu/sm5/
 
 #grab potential updates for stepmania.
 updateclient $_REMOTE_SM_PATH/ $_LOCAL_UPDATE_PATH/sm5 'Stepmania Software'
@@ -46,11 +46,11 @@ else
 	mkdir $_LOCAL_SM_PATH
 	cd $_LOCAL_SM_PATH
 	tar xf $_LOCAL_UPDATE_PATH/sm5/stepmania-build-current.tar.gz
-	rm -rf $_LOCAL_SM_PATH/Save
-	touch portable.ini
-	mkdir Save
-	cp -rf $_LOCAL_BACKUP_PATH/sm5.$_NOW/Save/* $_LOCAL_SM_PATH/Save/
-	cp -rsf $_LOCAL_SONG_PATH/* $_LOCAL_SM_PATH/Songs/
+#	rm -rf $_LOCAL_SM_PATH/Save
+#	touch portable.ini
+#	mkdir Save
+#	cp -rf $_LOCAL_BACKUP_PATH/sm5.$_NOW/Save/* $_LOCAL_SM_PATH/Save/
+#	cp -rsf $_LOCAL_SONG_PATH/* $_LOCAL_SM_PATH/Songs/
 	cp $_LOCAL_UPDATE_PATH/sm5/stepmania-build-current.md5sum $_LOCAL_UPDATE_PATH/installed/stepmania-build-current.md5sum
 #        ls -1 $_LOCAL_SM_PATH/Themes > $_LOCAL_SM_PATH/theme.exclude
 #	rsync -avr --exclude-from $_LOCAL_SM_PATH/theme.exclude $_LOCAL_BACKUP_PATH/sm5.$_NOW/Themes/ $_LOCAL_SM_PATH/Themes/
@@ -58,5 +58,5 @@ else
 fi
 
 #grab themes and drop them in PACKAGES directory. 
-updateclient $_REMOTE_THEME_PATH/ $_LOCAL_SM_PATH/Packages 'Stepmania Themes'
+updateclient $_REMOTE_THEME_PATH/ $_LOCAL_SM_SHARE_PATH/Packages 'Stepmania Themes'
 
